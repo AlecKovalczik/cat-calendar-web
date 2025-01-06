f40861182f3d6e57b5dcc051aa5d75865b029c76
AlecKovalczik <aleckovalczik@gmail.com>
2025-01-06 16:35:27 -0500

    attempting to seed data, but receiving a server error

---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,30 @@
+// This file contains placeholder data that will be replaced in launch.
+
+const tasks = [
+    {
+        id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+        title: "Task 1",
+        description: "Task 1 description.",
+        status: "Completed",
+    },
+    {
+        id: "76d65c26-f784-44a2-ac19-586678f7c2f2",
+        title: "Task 2",
+        description: "Task 2 description.",
+        status: "Completed",
+    },
+    {
+        id: "CC27C14A-0ACF-4F4A-A6C9-D45682C144B9",
+        title: "Task 3",
+        description: "Skip this task! This one sucks to do and you will have a lot more fun if you just decide to do the next one instead.",
+        status: "Incomplete",
+    },
+    {
+        id: "13D07535-C59E-4157-A011-F8D2EF4E0CBB",
+        title: "Kitten Cuddle Party",
+        description: "Cuddle up with Niko and THE BEAN!",
+        status: "Complete",
+    },
+];
+
+export { tasks };
--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,13 +1,43 @@
+// import bcrypt from "bcrypt";
+import { db } from "@vercel/postgres";
+import { tasks } from "@/app/lib/placeholder-data";
+
+const client = await db.connect();
+
+async function seedTasks() {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    await client.sql`DROP TABLE tasks;`
+    await client.sql`
+        CREATE TABLE IF NOT EXISTS tasks (
+            id UUID DEFAULT uuid_generate_v4 PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            status VARCHAR(255) DEFAULT "Incomplete"
+        );
+    `;
+
+    const insertedTasks = await Promise.all(
+        tasks.map(async (task) => {
+            return client.sql`
+                INSERT INTO tasks (id, title, description, status)
+                VALUES (${task.id}, ${task.title}, ${task.description}, ${task.status})
+                ON CONFLICT (id) DO NOTHING;
+            `;
+        }),
+    );
+
+    return insertedTasks;
+}
+
 export async function GET() {
     try {
-    //   await client.sql`BEGIN`;
-    //   await seedTasks();
-    //   await client.sql`COMMIT`;
-  
-      return Response.json({ message: 'Database seeded successfully' });
+        await client.sql`BEGIN`;
+        await seedTasks();
+        await client.sql`COMMIT`;
+
+        return Response.json({ message: 'Database seeded successfully' });
     } catch (error) {
-    //   await client.sql`ROLLBACK`;
-      return Response.json({ error }, { status: 500 });
+        await client.sql`ROLLBACK`;
+        return Response.json({ error }, { status: 500 });
     }
-  }
-  
+}
