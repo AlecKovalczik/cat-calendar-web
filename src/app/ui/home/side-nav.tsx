526936651dc348a843db50532526d0615370d662
AlecKovalczik <aleckovalczik@gmail.com>
2025-01-15 14:17:45 -0500

    Trying to figure out what is causing the error for Route in [auth].

--- src/app/ui/home/side-nav.tsx
+++ src/app/ui/home/side-nav.tsx
@@ -15,10 +15,10 @@ export default function SideNav() {
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
                 <NavLinks></NavLinks>
                 <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-                <link href="/api/auth/logout" className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 border border-gray-600 border-dashed p-3 text-sm font-medium hover:bg-red-200 hover:text-red-600 hover:border-red-600 hover:border-solid md:flex-none md:justify-start md:p-2 md:px-3">
+                <Link href="/api/auth/logout" className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 border border-gray-600 border-dashed p-3 text-sm font-medium hover:bg-red-200 hover:text-red-600 hover:border-red-600 hover:border-solid md:flex-none md:justify-start md:p-2 md:px-3">
                     {/* <PowerIcon className="w-6" /> */}
                     <div className="hidden md:block">Sign Out</div>
-                </link>
+                </Link>
             </div>
         </div>
     )
