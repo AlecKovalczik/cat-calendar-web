fa63ad07aab9bb881cfff1e2b1db3d49cc9b870e
AlecKovalczik <aleckovalczik@gmail.com>
2025-02-02 23:29:35 -0500

    changed the layout of the homepage a little bit, will probably add some stuff to make the landing page more interesting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Cat Calendar</h1>
+    <div className="w-fill mx-3 mt-3">
+      <main className="flex flex-row grow text-center gap-4 row-start-2 items-center sm:items-start p-4 border border-black rounded-md bg-violet-600">
+        <h1 className="h-fill text-center text-white text-3xl">Cat Calendar</h1>
+        <div className="grow"></div>
         <Link
           href="/login"
-          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          className="flex items-center gap-4 self-start rounded-lg bg-white text-black px-6 py-3 md:text-base"
         >
           <span>Log in</span>
         </Link>
         <Link
           href="/signup"
-          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          className="flex items-center gap-4 self-start rounded-lg bg-white text-black px-6 py-3 md:text-base"
         >
           <span>Sign up</span>
         </Link>
