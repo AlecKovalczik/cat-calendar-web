ff6f44460880f2b7eb57193cacceebca77ec162e
AlecKovalczik <aleckovalczik@gmail.com>
2025-03-04 15:34:26 -0500

    removed an extra line

--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -32,7 +32,6 @@ export default function SignupForm() {
       <div className="flex flex-row"></div>
       <Link href="/signup" className="text-black hover:text-violet-600">Create an account</Link>
       <button disabled={pending} type="submit" className="h-12 border border-dashed border-black rounded-md bg-gray-100 hover:border-solid hover:border-violet-600 hover:bg-violet-100 hover:text-violet-800">
-
         Log in
       </button>
     </form>
