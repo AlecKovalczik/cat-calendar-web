fd7d4ca7f9e6a22de9e9d487addcbcadfba89bbf
AlecKovalczik <aleckovalczik@gmail.com>
2025-03-04 10:47:14 -0500

    updated login form design

--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -1,35 +1,36 @@
 'use client';
- 
+
 import { login } from "@/app/actions/auth";
 import { useActionState } from "react";
- 
+
 export default function SignupForm() {
   const [state, action, pending] = useActionState(login, undefined)
- 
+
   return (
     <form action={action} className="flex grow flex-col w-full px-4 pb-4 text-xl">
-      <div>
+      <div className="px-8">
         <label htmlFor="username">Username:</label><br></br>
-        <input id="username" name="username" placeholder="Username" className="border border-black rounded-md bg-gray-100" />
+        <input id="username" name="username" placeholder="Username" className="w-full px-1 border border-black rounded-md bg-gray-100" />
+        {state?.errors?.username && <p className="text-red-600 text-sm">{state.errors.username}</p>}
       </div>
-      {state?.errors?.username && <p>{state.errors.username}</p>}
-      <div>
+      <div className="px-8">
         <label htmlFor="password">Password:</label><br></br>
-        <input id="password" name="password" type="password" placeholder="Password" className="border border-black rounded-md bg-gray-100" />
+        <input id="password" name="password" type="password" placeholder="Password" className="w-full px-1 border border-black rounded-md bg-gray-100" />
+        {state?.errors?.password && (
+          <div className="text-red-600 text-sm">
+            <p>Password must:</p>
+            <ul>
+              {state.errors.password.map((error) => (
+                <li key={error}>- {error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
-      {state?.errors?.password && (
-        <div>
-          <p>Password must:</p>
-          <ul>
-            {state.errors.password.map((error) => (
-              <li key={error}>- {error}</li>
-            ))}
-          </ul>
-        </div>
-      )}
       <div className="grow"></div>
       <div className="flex flex-row"></div>
-      <button disabled={pending} type="submit">
+      <button disabled={pending} type="submit" className="h-12 border border-dashed border-black rounded-md bg-gray-100 hover:border-solid hover:border-violet-600 hover:bg-violet-100 hover:text-violet-800">
+
         Login
       </button>
     </form>
