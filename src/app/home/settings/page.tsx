10e1739d7800c560c198ccb60677577ac7f12050
AlecKovalczik <aleckovalczik@gmail.com>
2025-03-05 18:15:28 -0500

    built out the ui structure

--- src/app/home/settings/page.tsx
+++ src/app/home/settings/page.tsx
@@ -1,14 +1,29 @@
 
 
 export default function SettingsPage() {
+
+
     return (
-        <div>
-            <header className="sticky top-0 w-fill pt-4 pb-2 px-12 mb-2 z-10 border-b border-dashed border-black"> {/*sticky top-0 pt-4 pb-2 px-12 mb-2 backdrop-blur border border-b-black border-dashed */}
+        <div className="h-screen">
+            <header className="w-full pt-4 pb-2 px-12 mb-2 z-10 border-b border-dashed border-black"> {/*sticky top-0 pt-4 pb-2 px-12 mb-2 backdrop-blur border border-b-black border-dashed */}
                 <div className="w-full flex grow flex-row items-center px-4 py-4 mb-2 text-white bg-violet-600 border border-black rounded-md shadow-[4px_4px_0_0_rgb(0,0,0,.3)] shadow-stone-400 z-50">
                     <h1 className="text-3xl uppercase"><b>Settings</b></h1>
                     <div className="hidden w-auto h-full grow rounded-md md:block"></div>
                 </div>
             </header>
+            <main className="flex flex-row space-x-2 h-4/5 mx-12 mt-4 p-4 bg-white border border-black rounded-md shadow-[4px_4px_0_0_rgb(0,0,0,.3)] shadow-stone-400">
+                <div className="w-1/3 py-2">
+                    <h2 className="text-center text-xl">Profile</h2>
+                </div>
+                <div className="h-full border-l border-dashed border-black "></div>
+                <div className="w-1/3 py-2">
+                    <h2 className="text-center text-xl">Theme</h2>
+                </div>
+                <div className="h-full border-r border-dashed border-black"></div>
+                <div className="w-1/3 py-2">
+                    <h2 className="text-center text-xl">Privacy</h2>
+                </div>
+            </main>
         </div>
     )
 }
